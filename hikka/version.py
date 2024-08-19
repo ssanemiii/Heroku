bb5ba4eb43efea2657f68d0ef86d4fd74f9dc176
Who? <155328415+coddrago@users.noreply.github.com>
2024-08-19 19:28:34 +0700

    Update version.py

--- hikka/version.py
+++ hikka/version.py
@@ -5,7 +5,7 @@
 # You can redistribute it and/or modify it under the terms of the GNU AGPLv3
 # 🔑 https://www.gnu.org/licenses/agpl-3.0.html
 
-__version__ = (1, 6, 5)
+__version__ = (1, 6, 4)
 
 import os
 
@@ -16,4 +16,4 @@ try:
         path=os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     ).active_branch.name
 except Exception:
-    branch = "Нерси делает хуйню в версиях"
+    branch = "master"
